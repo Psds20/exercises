3c5d07ccf1788fd123aed1e816e440e6c5f98753
Pedro Guilherme <89921744+Psds20@users.noreply.github.com>
2022-12-05 01:51:11 -0300

    little correction

--- Risca/ex020.py
+++ Risca/ex020.py
@@ -1,8 +1,8 @@
 from random import shuffle
-n1 = input("type the name of first's student: ")
-n2 = input("type the name of second's student: ")
-n3 = input("type the name of third's student: ")
-n4 = input("type the name of fourth's student: ")
+n1 = input("type the name of the first's student: ")
+n2 = input("type the name of the second's student: ")
+n3 = input("type the name of the third's student: ")
+n4 = input("type the name of the fourth's student: ")
 
 list1 = [n1, n2, n3, n4]
 shuffle(list1)
